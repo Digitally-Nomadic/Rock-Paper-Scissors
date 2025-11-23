a71fb0d77fc248843cbdc36e0439bb3f65fd27ac
Digitally-Nomadic <195157495+Digitally-Nomadic@user.noreply.github.com>
2025-11-23 11:19:31 -0500

    solved getComputerChoice with switch statement

--- odin-rps.js
+++ odin-rps.js
@@ -1,1 +1,15 @@
-console.log("Hewwo World");
+function getComputerChoice() {
+   
+    let comp = Math.floor(Math.random() * 3);
+        
+        switch(comp) {
+            case 0:
+                return ("Rock");
+            case 1:
+                return ("Scissors");
+            case 2:
+                return ("Paper");
+          }
+    
+}
+
