2364ff705ee0adc72f472a622e248dfc19355ed8
Digitally-Nomadic <195157495+Digitally-Nomadic@user.noreply.github.com>
2025-12-03 11:18:29 -0500

    starting to reapply call back functions hC and cC

--- odin-rps.js
+++ odin-rps.js
@@ -1,15 +1,36 @@
-function getComputerChoice() {
-   
-    let comp = Math.floor(Math.random() * 3);
-        
-        switch(comp) {
-            case 0:
-                return ("Rock");
-            case 1:
-                return ("Scissors");
-            case 2:
-                return ("Paper");
-          }
-    
+'use strict'
+
+function getHumanChoice() {
+    let hI = prompt('Jan!Ken!Pon!','');
+
+    let huIN = hI.toLowerCase();
+
+    if(huIN === 'rock') {
+        return 'Rock';
+    } else if (huIN === 'paper') {
+        return 'Paper';
+    } else if (huIN === 'scissors') {
+        return 'Scissors';
+    } else {
+        return undefined;
+    }
 }
 
+function getComputerChoice() {
+    let comp = Math.floor(Math.random() * 3);
+
+    switch(comp) {
+        case 0:
+            return ('Rock');
+            break;
+        case 1:
+            return ('Scissors');
+            break;
+        case 2:
+            return ('Paper');
+            break;
+
+
+
+    }
+}
