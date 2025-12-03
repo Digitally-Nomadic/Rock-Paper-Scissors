7c2d3ec4d38b4b12285cdeb46a1538fab441e9a2
Digitally-Nomadic <195157495+Digitally-Nomadic@user.noreply.github.com>
2025-12-03 13:57:36 -0500

    encountered error with playRound function return

--- odin-rps.js
+++ odin-rps.js
@@ -34,3 +34,45 @@ function getComputerChoice() {
 
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+    
+    let shoot = humanChoice && computerChoice; 
+    let lose1 = humanChoice == 'Rock' && computerChoice == 'Paper';
+    let win1 = humanChoice == 'Rock' && computerChoice == 'Scissors';
+    let draw1 = humanChoice == 'Rock' && computerChoice == 'Rock';
+    let lose2 =  humanChoice == 'Paper' && computerChoice == 'Scissors';
+    let win2 = humanChoice == 'Paper' && computerChoice == 'Rock';
+    let draw2 = humanChoice == 'Paper' && computerChoice == 'Paper';
+    let lose3 = humanChoice == 'Scissors' && computerChoice == 'Rock';
+    let win3 = humanChoice == 'Scissors' && computerChoice == 'Paper';
+    let draw3 = humanChoice == 'Scissors' && computerChoice == 'Scissors';
+     
+    switch (shoot) {
+        case lose1:
+            return console.log('You lose. Paper beats Rock.');
+            break;
+        case lose2:
+            return console.log('You lose. Scissor beats Paper.');
+            break;
+        case lose3:
+            return console.log('You lose. Rock beats Scissors.');
+            break;
+        case win1:
+            return console.log('You win. Rock beats Scissors.');
+            break;
+        case win2:
+            return console.log('You win. Paper beats Rock');
+            break;
+        case win3:
+            return console.log('You win. Scissors beats Paper');
+        case draw1:
+        case draw2:
+        case draw3:
+            return console.log('Draw. Try again.');
+            break;
+    } 
+} 
+
+let playerOption = getHumanChoice();
+let computerOption = getComputerChoice();
